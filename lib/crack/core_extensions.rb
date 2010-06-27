8452c74a97fea09a8ada205e5e7538138ea11c1e
Daniël van de Burgt <daniel [at] daandev [dot] net>
2010-06-27 16:07:09 +0200

    Made sure Object.blank? is loaded after defining the more specific blank? methods

--- lib/crack/core_extensions.rb
+++ lib/crack/core_extensions.rb
@@ -1,18 +1,5 @@
 require 'uri'
 
-class Object #:nodoc:
-  # @return <TrueClass, FalseClass>
-  #
-  # @example [].blank?         #=>  true
-  # @example [1].blank?        #=>  false
-  # @example [nil].blank?      #=>  false
-  # 
-  # Returns true if the object is nil or empty (if applicable)
-  def blank?
-    nil? || (respond_to?(:empty?) && empty?)
-  end unless method_defined?(:blank?)
-end # class Object
-
 class Numeric #:nodoc:
   # @return <TrueClass, FalseClass>
   # 
@@ -65,6 +52,19 @@ class String #:nodoc:
     return $+.downcase
   end unless method_defined?(:snake_case)
 end # class String
+
+class Object #:nodoc:
+  # @return <TrueClass, FalseClass>
+  #
+  # @example [].blank?         #=>  true
+  # @example [1].blank?        #=>  false
+  # @example [nil].blank?      #=>  false
+  # 
+  # Returns true if the object is nil or empty (if applicable)
+  def blank?
+    nil? || (respond_to?(:empty?) && empty?)
+  end unless method_defined?(:blank?)
+end # class Object
 
 class Hash #:nodoc:
   # @return <String> This hash as a query string
